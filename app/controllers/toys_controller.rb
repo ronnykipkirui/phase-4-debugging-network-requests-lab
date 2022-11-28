238a9813e43bb960c5fa3f3f07713b115f350cd8
Ronny <ronnybet41@gmail.com>
2022-11-28 11:34:07 +0300

    completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,31 +1,35 @@
+
 class ToysController < ApplicationController
-  wrap_parameters format: []
-
-  def index
-    toys = Toy.all
-    render json: toys
-  end
-
-  def create
-    toy = Toys.create(toy_params)
-    render json: toy, status: :created
-  end
-
-  def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
-  end
-
-  def destroy
-    toy = Toy.find_by(id: params[:id])
-    toy.destroy
-    head :no_content
-  end
-
-  private
+  # rescue_from ActiveRecord::I
+    wrap_parameters format: []
   
-  def toy_params
-    params.permit(:name, :image, :likes)
+    def index
+      toys = Toy.all
+      render json: toys
+    end
+  
+    def create
+      toy = Toy.create!(toy_params)
+      render json: toy, status: :created
+    end
+  
+    def update
+      toy = Toy.find_by(id: params[:id])
+      toy.update(toy_params)
+      render json: toy
+    end
+  
+    def destroy
+      toy = Toy.find_by(id: params[:id])
+      toy.destroy
+      head :no_content
+    end
+  
+    private
+    
+    def toy_params
+      params.permit(:name, :image, :likes)
+    end
+  
   end
-
-end
+  
